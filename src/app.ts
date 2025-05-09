caeea4784902ba61faa8ca755ba6a114e9e5a7fb
Chethan S <chethansgowda95@gmail.com>
2025-05-09 19:42:02 +0530

    removed interservice app

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,33 @@
-import * as bodyParser from 'body-parser';
-import express from 'express';
-import { 
-    Index, 
-    // InterServiceIndex 
-} from './routes/index';
-
+import * as bodyParser from "body-parser";
+import express from "express";
+import {
+  Index,
+  // InterServiceIndex
+} from "./routes/index";
 
 class App {
-    public app: express.Application;
-    public indexRoutes: Index = new Index();
+  public app: express.Application;
+  public indexRoutes: Index = new Index();
 
-    constructor() {
-        this.app = express();
-        this.app.use(bodyParser.json());
-        this.app.use(express.urlencoded({extended: true}));
-        this.indexRoutes.routes(this.app);
-    }
+  constructor() {
+    this.app = express();
+    this.app.use(bodyParser.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.indexRoutes.routes(this.app);
+  }
 }
 
-class InterServiceApp {
-    public interServiceApp: express.Application;
-    // public interServiceRoutes: InterServiceIndex = new InterServiceIndex();
+// class InterServiceApp {
+//     public interServiceApp: express.Application;
+//     // public interServiceRoutes: InterServiceIndex = new InterServiceIndex();
 
-    constructor() { 
-        this.interServiceApp = express();
-        this.interServiceApp.use(bodyParser.json());
-        // this.interServiceRoutes.routes(this.interServiceApp);
-    }
-}
+//     constructor() {
+//         this.interServiceApp = express();
+//         this.interServiceApp.use(bodyParser.json());
+//         // this.interServiceRoutes.routes(this.interServiceApp);
+//     }
+// }
 
-export const interServiceComs = new InterServiceApp().interServiceApp;
+// export const interServiceComs = new InterServiceApp().interServiceApp;
 
 export default new App().app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import app, { interServiceComs } from './app';
+import app from "./app";
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
-interServiceComs.listen(process.env.INTERSERVICES_PORT, () => { 
-    console.log(`Inter-service communication server is running on port ${process.env.INTERSERVICES_PORT}`); 
-});
+// interServiceComs.listen(process.env.INTERSERVICES_PORT, () => {
+//     console.log(`Inter-service communication server is running on port ${process.env.INTERSERVICES_PORT}`);
+// });
